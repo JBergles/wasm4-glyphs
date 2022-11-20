42fd8ff6c28eb6c949a87fcbaddbb032a7ea3999
Jim Bergstrom <jim@Jims-MacBook-Air.local>
2022-11-19 23:08:47 -0700

    Add a couple comments

--- main.go
+++ main.go
@@ -10,13 +10,16 @@ func writeChar() {
 	op := rand.Intn(100)
 	x, y := rand.Intn(20)*8, rand.Intn(20)*8
 	if op < 10 {
+		//Clear a character by filling with background
 		*w4.DRAW_COLORS = 0x11
 		w4.Rect(x, y, 8, 8)
 	} else if op < 15 {
+		//Write a character with a transparent background
 		buf[0] = byte(rand.Intn(0x5D) + 0x21)
 		*w4.DRAW_COLORS = 0x02 + uint16(rand.Intn(3))
 		w4.Text(string(buf[:]), x, y)
 	} else {
+		//Write a character with a solid background
 		buf[0] = byte(rand.Intn(0x5D) + 0x21)
 		*w4.DRAW_COLORS = 0x12 + uint16(rand.Intn(3))
 		w4.Text(string(buf[:]), x, y)
@@ -33,10 +36,12 @@ func start() {
 func update() {
 	gamepad := *w4.GAMEPAD1
 	if gamepad&w4.BUTTON_1 != 0 {
+		//Clear the screen by filling with background color
 		*w4.DRAW_COLORS = 0x11
 		w4.Rect(0, 0, 160, 160)
 	}
 	if gamepad&w4.BUTTON_2 != 0 {
+		//Randomize color palette
 		w4.PALETTE[0] = rand.Uint32()
 		w4.PALETTE[1] = rand.Uint32()
 		w4.PALETTE[2] = rand.Uint32()
